f9cbc2ab63d74227bb3f10be362e7a0ad67ea62f
karpagam <karpagam-s@pluralsight.com>
2024-05-09 10:42:21 +0530

    maths excercise

---
+++ MathScripts/tips.js
@@ -0,0 +1,16 @@
+// sample inputs
+let foodCost = 79.25;
+let tax = 6.54;
+let tip = 12.00;
+
+// calculations
+let totalDue = foodCost + tax + tip;
+
+// output
+//console.log("The total due is " + totalDue);
+
+console.log(
+    "Food cost is " + foodCost + " and tax is " + tax);
+    console.log("Tip is " + tip.toFixed(2));
+    console.log("Total Due is " + totalDue);
+ 
